36d7a1729e89980a524b87c1007376ea3a7a0d6f
KM <40517079+nisinosora@users.noreply.github.com>
2021-10-06 23:28:09 +0900

    updata6
    画面サイズが小さい時は操作不能に変更

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ document.addEventListener('DOMContentLoaded',function(){
     lists.forEach(function($value){
       var $link = $value;
       var $src = $link.src
-      $link.width = '560';
-      $link.height = '315';
       $link.src = `${$src}?controls=1&rel=0`;
       $link.title = 'YouTube video player';
       $link.setAttribute('frameborder', "0")
@@ -13,6 +11,15 @@ document.addEventListener('DOMContentLoaded',function(){
     });
   });
 });
+
+window.addEventListener('resize', function(){
+  var lists  = document.querySelectorAll("iframe");
+    lists.forEach(function($value){
+      if(parseInt($value.width) < 430){
+        $value.src = `${$value.src.replace("?controls=1&rel=0", "")}?controls=0&rel=0`;
+      }
+    });
+},false)
 
 $(function(){
   $('.single-item').slick({
