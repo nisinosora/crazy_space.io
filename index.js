d0eba4a007f1fad87de5101ff7c086052194b1b1
KM <40517079+nisinosora@users.noreply.github.com>
2021-10-10 22:08:31 +0900

    delete/resize_js
    処理軽減のためリサイズした際の機能を削除

--- index.js
+++ index.js
@@ -12,15 +12,6 @@ document.addEventListener('DOMContentLoaded',function(){
   });
 });
 
-window.addEventListener('resize', function(){
-  var lists  = document.querySelectorAll("iframe");
-    lists.forEach(function($value){
-      if(parseInt($value.width) < 430){
-        $value.src = `${$value.src.replace("?controls=1&rel=0", "")}?controls=0&rel=0`;
-      }
-    });
-},false)
-
 $(function(){
   $('.single-item').slick({
     dots: true,
